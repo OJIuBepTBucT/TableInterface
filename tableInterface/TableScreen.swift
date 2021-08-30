1762f9cea7cc8daeafecd18dd54a364320ac88bf
anton <->
2021-08-30 21:46:00 +0500

    добавил градиент

--- tableInterface/TableScreen.swift
+++ tableInterface/TableScreen.swift
@@ -1,9 +1,20 @@
 import Foundation
 import UIKit
 
-class TableScreen:ViewController {
+class TableScreen: UIViewController {
+    let gradientL = CAGradientLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        setupGradient()
     }
+    
+    func setupGradient() {
+        gradientL.frame = self.view.bounds
+        gradientL.colors = [UIColor.blue.cgColor, UIColor.white.cgColor]
+        gradientL.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientL.endPoint = CGPoint (x: 0.0, y: 1.0)
+        self.view.layer.insertSublayer(gradientL, at: 0)
+    }
+  
+    
 }
